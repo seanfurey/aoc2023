93bf1a446bd25e8e7b596e34d43dc3141bb1400f
Sean Furey <sean@furey.me.uk>
2023-12-03 22:45:53 +0000

    dqy 3 work

--- 3/main.cpp
+++ 3/main.cpp
@@ -29,12 +29,12 @@ bool is_symbol(char c)
 bool beside_symbol(int x, int y)
 {
     int tlx = x-1;
-    int tly = y-stride;
+    int tly = y;//-stride;
     int brx = x+1;
-    int bry = y+stride;
+    int bry = y;//+stride;
     if (tlx < 0) tlx+=1;
-    if (tly < 0) tly+=stride;
-    if (bry >= size) bry-=stride;
+//    if (tly < 0) tly+=stride;
+//    if (bry >= size) bry-=stride;
 for (int xp=tlx; xp<=brx; xp++)
 for (int yp=tly; yp<=bry; yp++)
 if (is_symbol(map[xp+yp])) return true;
